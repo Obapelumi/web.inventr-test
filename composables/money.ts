46b16d37dd50ce525968c47497c300e909e5872f
Emmanuel Olomolaiye <31254017+Obapelumi@users.noreply.github.com>
2023-04-26 08:25:14 +0100

    internationalization

--- composables/money.ts
+++ composables/money.ts
@@ -1,19 +1,21 @@
 import { Money } from './resources/finance'
 
 const style = 'currency'
+const locale = [...navigator.languages]
 
 export const getCurrencySymbol = (currency: Money['currency']) => {
   return (
-    new Intl.NumberFormat('en-NG', { style, currency })
+    new Intl.NumberFormat(locale, { style, currency })
       .formatToParts(0)
       .find(({ type }) => type === 'currency')?.value || ''
   )
 }
 
 export const toMoney = (money: Money) =>
-  new Intl.NumberFormat('en-US', { style, currency: money.currency }).format(
-    money.amount
-  )
+  new Intl.NumberFormat(locale, {
+    style,
+    currency: money.currency
+  }).format(money.amount)
 
 export const decimalPlaces = (value: number, decimalPlaces: number) => {
   const denominator = Math.pow(10, decimalPlaces)
